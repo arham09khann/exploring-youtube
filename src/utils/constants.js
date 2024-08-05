d2c11f06fb3f976de007322334eb87cfedb5e707
Mohd Arham Khan <68265321+arham09khann@users.noreply.github.com>
2024-08-05 15:52:02 +0530

    Update constants.js
    removied Api keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-// const GOOGLE_API_KEY = "AIzaSyDjh32Bf2MJFqYXCEg9Xwf3JzVha0r1vGw";
-// const GOOGLE_API_KEY = "AIzaSyA4VxEBXzrfd_nKKMT9gdRnKJGSH8EVjCs";
+
 
 export const Youtube_api =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=";
